c7efbed7cafb404feab845e06b21713ca6257d4b
Jake Fontaine <Fontaine.JakeA@gmail.com>
2026-04-26 15:23:51 -0400

    Host is dynamic now, change some colors

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const app = express();
 app.use(express.json());
 app.use('/', express.static(path.join(__dirname, './')));
 
-const HOST = '0.0.0.0';
+const { networkInterfaces } = require('os');
+const HOST = Array.from(Object.values(networkInterfaces())).flat().find(net => net.family === 'IPv4' && !net.internal).address;
 const PORT = 3000;
 
 app.get('/', (req, res) => res.json({ success: true, message: "We're fucking doing this shit" }));
@@ -12,4 +13,4 @@ app.get('/', (req, res) => res.json({ success: true, message: "We're fucking doi
 app.get('/frame', (req, res) => res.sendFile(path.join(__dirname, '/frame/index.html')));
 app.get('/sender', (req, res) => res.sendFile(path.join(__dirname, '/sender/index.html')));
 
-app.listen(PORT, '192.168.0.12', () => console.log(`Running on port ${PORT}`));
+app.listen(PORT, HOST, () => console.log(`Running on port ${PORT}`));
