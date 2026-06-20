cb36d9633c0a991355b15603c6e938b6614ebd10
Jake Fontaine <Fontaine.JakeA@gmail.com>
2026-06-20 11:56:41 -0400

    removing test wifi

--- www/frame/components/SettingsPopup.js
+++ www/frame/components/SettingsPopup.js
@@ -498,32 +498,32 @@ export default class SettingsPopup extends HTMLElement{
 
     async loadWifiPanel(){
         const wifiResponse = await sendRequest('/frame/wifi');
-        const a = [{
-            ssid: 'network 1',
-            CONNECTED: true,
-            quality: 80,
-            security: 'WP0'
-        },{
-            ssid: 'network 2',
-            CONNECTED: false,
-            quality: 50,
-            security: 'WP0'
-        },{
-            ssid: 'network 3',
-            CONNECTED: false,
-            quality: 30,
-            security: ''
-        },{
-            ssid: 'network 4',
-            CONNECTED: false,
-            quality: 70,
-            security: 'WP0'
-        }];
+        // const a = [{
+        //     ssid: 'network 1',
+        //     CONNECTED: true,
+        //     quality: 80,
+        //     security: 'WP0'
+        // },{
+        //     ssid: 'network 2',
+        //     CONNECTED: false,
+        //     quality: 50,
+        //     security: 'WP0'
+        // },{
+        //     ssid: 'network 3',
+        //     CONNECTED: false,
+        //     quality: 30,
+        //     security: ''
+        // },{
+        //     ssid: 'network 4',
+        //     CONNECTED: false,
+        //     quality: 70,
+        //     security: 'WP0'
+        // }];
 
         const wifiList = this.querySelector('.wifi-panel .list-inner');
         while(wifiList.firstChild){ wifiList.firstChild.remove(); }
 
-        for(const network of a/*wifiResponse.networks*/){
+        for(const network of wifiResponse.networks){
             const newWifiEntry = new WifiEntry(network);
 
             newWifiEntry.addEventListener('reload', () => this.loadWifiPanel());
